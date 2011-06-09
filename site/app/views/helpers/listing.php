431b57df6bee658fae2187b99c7e282bf0361c0c
Florian Queze <florian@instantbird.org>
2011-06-09 23:31:10 +0200

    Bug 825 - upload of add-ons failed to report their result because of trailing commas in the JSON response, r=Even.

--- site/app/views/helpers/listing.php
+++ site/app/views/helpers/listing.php
@@ -85,20 +85,24 @@ class ListingHelper extends Helper
     function json($json, $tab = 0, $encode = false) {
         echo "{\n";
         if (!empty($json)) {
+            $first = true;
             foreach ($json as $key => $value) {
+                if (!$first)
+                    echo ",\n";
                 echo str_repeat("\t", $tab)."\"{$key}\": ";
                 if (is_array($value))
                     $this->json($value, $tab + 1, $encode);
                 else
                     if (is_numeric($value) && strpos($value, '.') === false)
-                        echo "{$value},\n";
+                        echo "{$value}";
                     elseif ($encode)
-                        echo '"'.urlencode($value).'",'."\n";
+                        echo '"'.urlencode($value).'"';
                     else
-                        echo "\"{$value}\",\n";
+                        echo "\"{$value}\"";
+                $first = false;
             }
         }
-        echo str_repeat("\t", $tab).'}'.($tab == 0 ? '' : ',')."\n";
+        echo str_repeat("\t", $tab)."}\n";
     }
     
 }
